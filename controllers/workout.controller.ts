0e292ef35f862191aa52c141035b87fee795ac31
Riad Mefti <104847379+RiadMefti@users.noreply.github.com>
2024-12-23 14:10:17 -0500

    crudd workout in controller

--- controllers/workout.controller.ts
+++ controllers/workout.controller.ts
@@ -1,23 +1,67 @@
-import { Request, Response, NextFunction } from "express";
-import { CreateApiSuccess } from "../utils/helpers";
-import { authService } from "../services/auth.service";
+import { Request, Response } from "express";
+import { CreateApiSuccess, CreateApiError } from "../utils/helpers";
+import { Workout } from "../schemas/workout.schema";
 
 class WorkoutController {
   constructor() { }
 
-  public async getWorkouts(req: Request, res: Response): Promise<void> {
 
+  //get all the workouts
+  public async getWorkouts(req: Request, res: Response): Promise<void> {
+    const workouts = await Workout.find({ user: req.user._id });
+    CreateApiSuccess(workouts.map(w => w.toDTO()), 200, res);
   }
+  //get a workout by id
   public async getWorkout(req: Request, res: Response): Promise<void> {
+    const workout = await Workout.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
+    if (!workout) {
+      return CreateApiError("Workout not found", 404, res);
+    }
+
+    CreateApiSuccess(workout.toDTO(), 200, res);
   }
+
   public async createWorkout(req: Request, res: Response): Promise<void> {
+    const workout = new Workout({
+      ...req.body,
+      user: req.user._id
+    });
+
+    await workout.save();
+    CreateApiSuccess(workout.toDTO(), 201, res);
   }
+
   public async editWorkout(req: Request, res: Response): Promise<void> {
+    const workout = await Workout.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
+    if (!workout) {
+      return CreateApiError("Workout not found", 404, res);
+    }
+
+    const updated = await workout.edit(req.body);
+    CreateApiSuccess(updated, 200, res);
   }
+
   public async deleteWorkout(req: Request, res: Response): Promise<void> {
+    const workout = await Workout.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    });
+
+    if (!workout) {
+      return CreateApiError("Workout not found", 404, res);
+    }
+
+    await workout.delete();
+    CreateApiSuccess({ message: "Deleted" }, 200, res);
   }
 }
+
 export const workoutController = new WorkoutController();
-
-
-
--- middlewares/authorization.middleware.ts
+++ middlewares/authorization.middleware.ts
@@ -1,14 +1,24 @@
 import { Request, Response, NextFunction } from "express";
-import { CreateApiError, HandleError } from "../utils/helpers";
+import { CreateApiError } from "../utils/helpers";
 import { authService } from "../services/auth.service";
+import { UserDTO } from "../types/user.types";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: UserDTO;
+    }
+  }
+}
 
 export function Authorization(req: Request, res: Response, next: NextFunction) {
   // List of routes to exclude from the middleware
   const excludedRoutes = ["/auth/login", "/auth/register", "/health"];
-
   if (excludedRoutes.includes(req.path)) {
     return next();
   }
+
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer")) {
     return CreateApiError("Unauthorized", 401, res);
@@ -16,13 +26,14 @@ export function Authorization(req: Request, res: Response, next: NextFunction) {
 
   const token = authHeader.split(" ")[1];
   try {
-    const decoded = authService.verifyJWT(token);
-
-    if (!decoded) {
+    const valid = authService.verifyJWT(token);
+    if (!valid) {
       return CreateApiError("Unauthorized", 401, res);
-    } else {
-      next();
     }
+
+    const decoded = jwt.decode(token) as UserDTO;
+    req.user = decoded;
+    next();
   } catch (error) {
     return CreateApiError("Unauthorized", 401, res);
   }
